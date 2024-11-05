2327d21fef73d293d56afeb30468346411ce0611
BadgerBoy0535 <widske@gmail.com>
2024-11-05 16:21:34 +0200

    Created new folders and stuff, init my own branch.

--- client/src/screens/AppRouter.js
+++ client/src/screens/AppRouter.js
@@ -4,6 +4,7 @@ import Footer from "./shared/Footer/Footer";
 import NotFound from "./shared/404/NotFound";
 import HomeScreen from "./home/home/HomeScreen";
 import AboutUs from "./home/aboutscreen/AboutUs";
+import ContactUs from "./home/contactus/ContactUs";
 
 export default function AppRouter() {
   return (
@@ -13,6 +14,7 @@ export default function AppRouter() {
         <Routes>
           <Route path='/' element={<HomeScreen />}/>
           <Route path='/about-us' element={<AboutUs />}/>
+          <Route path='/contact-us' element={<ContactUs />}/>
           <Route path='/404' element={<NotFound />} />
           <Route path='*' element={<Navigate to='/404' />} />
         </Routes>
