fd813bececc5373a9d085b56e9592e0e9df11c74
CameronCS <camcstocks@gmail.com>
2024-11-05 14:25:31 +0200

    Updated Nav Bars

--- client/src/screens/shared/Nav/EmployeeNav/EmployeeNav.js
+++ client/src/screens/shared/Nav/EmployeeNav/EmployeeNav.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import '../Nav.css'; // Import your CSS file
 
-export default function EmployeeNav() {
+export default function Default() {
   return (
-    <div>EmployeeNav</div>
-  )
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item"><a href="/emp/" className="nav-link">Home</a></li>
+        <li className="nav-item"><a href="/emp/view-all" className="nav-link">View All Claims</a></li>
+        <li className="nav-item"><a href="/emp/logout" className="nav-link">FAQ</a></li>
+      </ul>
+    </nav>
+  );
 }
--- client/src/screens/shared/Nav/UserNav/UserNav.js
+++ client/src/screens/shared/Nav/UserNav/UserNav.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../Nav.css'; // Import your CSS file
 
-export default function UserNav() {
+export default function Default() {
   return (
-    <div>UserNav</div>
-  )
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item"><a href="/user/" className="nav-link">Home</a></li>
+        <li className="nav-item"><a href="/user/view-all" className="nav-link">View All</a></li>
+        <li className="nav-item"><a href="/user/view-submitted" className="nav-link">FAQ</a></li>
+        <li className="nav-item"><a href="/user/create" className="nav-link">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
 }
